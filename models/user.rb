b0bfceeb72d2e50a30b27a185f58f302ff3cb943
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-02-22 20:06:27 +0900

    move crypt to module

--- models/user.rb
+++ models/user.rb
@@ -1,23 +1,7 @@
-require 'digest/sha2'
-require 'openssl'
 
 module Model
   class User < Model::Base
-    def crypt_password(password)
-      Digest::SHA256.hexdigest(PASSWORD_SALT + password)
-    end
-
-    def decrypt_email(address)
-      dec = OpenSSL::Cipher::Cipher.new("aes-256-cbc")
-      dec.decrypt.pkcs5_keyivgen(address)
-      dec.update(PASSWORD_SALT) + dec.final
-    end
-
-    def encrypt_email(address)
-      enc = OpenSSL::Cipher::Cipher.new("aes-256-cbc")
-      enc.encrypt.pkcs5_keyivgen(address)
-      enc.update(PASSWORD_SALT) + enc.final
-    end
+    include Crypt
 
     def initialize(params)
       super(:users, params, [:name, :nickname, :email, :password])
