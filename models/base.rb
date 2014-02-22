bfb3db0dc28359d95879cc816728b99c97be41b9
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-02-22 19:26:18 +0900

    add shave attribute

--- models/base.rb
+++ models/base.rb
@@ -1,7 +1,10 @@
 #-*- encoding: utf-8
+require './helpers/validation.rb'
 
 module Model
   class ModelBase
+    include Validation
+    
     def check_accept(params, acceptance)
       raise_flag = false
       acceptance.each do |key|
@@ -11,16 +14,36 @@ module Model
       end
     end
 
+    def validate
+      @validate.each do |v|
+        v.call
+      end
+    end
+
+    def validates(type, key, parameter)
+
+    end
+
     def initialize(table, params, acceptance)
       @table = table
       @attr = Hash.new
+      @acceptance = acceptance
+      @validate = []
 
       check_accept(params, acceptance)
     end
 
-    
+    def shave_attribute
+      buffer = Hash.new
+      @acceptance.each do |key|
+        buffer[key] = @attr[key]
+      end
+      @attr = buffer
+    end
 
     def insert!
+      shave_attribute
+      validate
       DB[@table].insert(@attr)
     end
   end
