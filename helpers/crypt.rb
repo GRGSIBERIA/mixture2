0942d07057412677eb0ea156142133c7976ad3e7
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-02-22 20:05:37 +0900

    add crypt module

---
+++ helpers/crypt.rb
@@ -0,0 +1,20 @@
+require 'openssl'
+require "digest/sha2"
+
+module Crypt
+  def crypt_password(password)
+    Digest::SHA256.hexdigest(PASSWORD_SALT + password)
+  end
+
+  def decrypt_email(address)
+    dec = OpenSSL::Cipher::Cipher.new("aes-256-cbc")
+    dec.decrypt.pkcs5_keyivgen(address)
+    dec.update(PASSWORD_SALT) + dec.final
+  end
+
+  def encrypt_email(address)
+    enc = OpenSSL::Cipher::Cipher.new("aes-256-cbc")
+    enc.encrypt.pkcs5_keyivgen(address)
+    enc.update(PASSWORD_SALT) + enc.final
+  end
+end
