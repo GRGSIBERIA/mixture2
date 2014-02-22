23a631cd5288bd9187f7631f31715e11833c44aa
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-02-22 20:16:07 +0900

    add code

--- helpers/validation.rb
+++ helpers/validation.rb
@@ -1,11 +1,13 @@
 #-*- encoding: utf-8
 
 module Validation
-  def not_null(params)
-    
+  def not_null(cname, attribute, key)
+    if param.nil? then
+      raise ArgumentError, "#{cname}の#{key.to_s}が未入力です"
+    end
   end
 
-  def not_blank(params)
+  def not_blank(param)
     
   end
 end
