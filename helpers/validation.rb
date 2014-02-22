c566790c1c35eb865545e2c3ed45bfbb81af787c
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-02-22 20:23:44 +0900

    add blank check

--- helpers/validation.rb
+++ helpers/validation.rb
@@ -2,12 +2,14 @@
 
 module Validation
   def not_null(cname, attribute, key)
-    if param.nil? then
-      raise ArgumentError, "#{cname}の#{key.to_s}が未入力です"
+    if attribute[key].nil? then
+      raise ArgumentError, "#{cname}の#{key.to_s}がnilです"
     end
   end
 
-  def not_blank(param)
-    
+  def not_blank(cname, attribute, key)
+    if attribute[key].blank? then
+      raise ArgumentError, "#{cname}の#{key.to_s}が空です"
+    end
   end
 end
