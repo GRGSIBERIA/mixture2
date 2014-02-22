f4c148188f54b2b9b6a0c129d2f508bae07e8210
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-02-22 20:41:50 +0900

    add parameters

--- helpers/validation.rb
+++ helpers/validation.rb
@@ -1,15 +1,18 @@
 #-*- encoding: utf-8
 
 module Validation
-  def not_null(cname, attribute, key)
+  def not_null(cname, attribute, key, params)
     if attribute[key].nil? then
       raise ArgumentError, "#{cname}の#{key.to_s}がnilです"
     end
   end
 
-  def not_blank(cname, attribute, key)
+  def not_blank(cname, attribute, key, params)
     if attribute[key].blank? then
       raise ArgumentError, "#{cname}の#{key.to_s}が空です"
     end
   end
+
+  module_function :not_null
+  module_function :not_blank
 end
