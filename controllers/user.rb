bab8c62aba0e039144cebedd070caf0d3e537dbc
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-02-22 17:48:24 +0900

    move controller to models

--- controllers/user.rb
+++ controllers/user.rb
@@ -3,19 +3,10 @@ require './models/user.rb'
 
 module Controller
   class User
-    def self.crypt_password(password)
-      Digest::SHA256.hexdigest(PASSWORD_SALT + password)
-    end
+    
 
     def self.create(params)
-      user = {
-        password: crypt_password(params[:password]),
-        name: params[:username],
-        nickname: params[:nickname],
-        email: params[:email]
-      } 
-      Model::User
-
+      user = Model::User.new params
     end
   end
 end
