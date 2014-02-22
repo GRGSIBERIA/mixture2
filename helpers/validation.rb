961e672ca87797259b9859e3cfd469d37b05e41d
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-02-22 21:51:44 +0900

    add length validation

--- helpers/validation.rb
+++ helpers/validation.rb
@@ -13,10 +13,20 @@ module Validation
     end
   end
 
-  def max_length(cname, attribute, key, params)
-    
+  def length(cname, attribute, key, params)
+    if params.key?(:max) then
+      if attribute[key].length > params[:max] then
+        raise ArgumentError, "#{cname}の#{key.to_s}の長さが#{params[:max]}より大きいです"
+      end
+    end
+    if params.key?(:min) then
+      if attribute[key].length < params[:min] then
+        raise ArgumentError, "#{cname}の#{key.to_s}の長さが#{params[:min]}より小さいです"
+      end
+    end
   end
 
   module_function :not_null
   module_function :not_blank
+  module_function :length
 end
