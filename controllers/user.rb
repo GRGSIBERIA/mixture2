2532a5bb739e04918206636555ebec9dcb2dc4e1
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-02-22 17:25:12 +0900

    halfway

--- controllers/user.rb
+++ controllers/user.rb
@@ -1,7 +1,21 @@
+require 'digest/sha2'
+require './models/user.rb'
+
 module Controller
   class User
+    def self.crypt_password(password)
+      Digest::SHA256.hexdigest(PASSWORD_SALT + password)
+    end
+
     def self.create(params)
-      
+      user = {
+        password: crypt_password(params[:password]),
+        name: params[:username],
+        nickname: params[:nickname],
+        email: params[:email]
+      } 
+      Model::User
+
     end
   end
 end
