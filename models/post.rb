fdd7ee289272f13d37e774625e6e3e829d98977d
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-02-25 05:10:08 +0900

    add tags in posts

--- models/post.rb
+++ models/post.rb
@@ -15,4 +15,12 @@ class Post < Sequel::Model
   def self.find(id)
     DB[:posts].where(id: id.to_i).first
   end
+
+  def self.tags(id, page_num=0)
+    # これで動くか全くわからない, 要テスト
+    post_tags = DB[:post_tags]
+      .where(post_id: id.to_i)
+      .join_table(:inner, DB[:tags], id: :tag_id)
+    # 吐出されるハッシュが不明なので要検証
+  end
 end
