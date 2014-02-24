6ea88c91f3b7319d8975579c7e2cb87454999315
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-02-25 00:42:02 +0900

    move routing

--- app.rb
+++ app.rb
@@ -29,7 +29,7 @@ require './models/post_tag.rb'
 
 require './helpers/verify.rb' # DBを利用するためモデルの後に読み込む
 
-#require './routing/user.rb'
+require './routing/user.rb'
 
 
 
@@ -43,43 +43,8 @@ class Server < Sinatra::Base
   get '/' do 
     slim :index
   end
-
-  get '/user/new' do 
-    slim :new_user
-  end
-
-  get '/user/succeed' do 
-    slim :create_user_succeed
-  end
-
-  post '/user/create' do 
-    @user = User.add(params)
-
-    @user.validate
-    unless @user.valid? then
-      @errors = @user.errors
-      @username = params[:user_name]
-      @nickname = params[:nickname]
-      puts "Invalid Parameters!"
-      slim :new_user
-    else
-      #puts @user[:nickname].encoding
-      @user.save
-      session[:user_name] = @user.name
-      session[:apikey] = Crypt.make_apikey(@user)
-      redirect '/user/succeed'
-    end
-  end
-
-  get '/user/list' do 
-    @users = DB[:users].all
-    slim :user_list
-  end
-
-  get '/user/:id' do 
-
-  end
-
+  
+  routing_user
 
 end
 
