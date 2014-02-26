2320a22cf21d90d8d177998a5582b6075e3fa3a8
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-02-27 01:45:03 +0900

    confirm upload

--- routing/post.rb
+++ routing/post.rb
@@ -15,17 +15,17 @@ def routing_post
 }
 EOS
  
-    policy = Base64.encode64(policy_document).gsub("\n","")
-    @policy = "policy: #{policy}"
+    @policy = Base64.encode64(policy_document).gsub("\n","")
      
     aws_secret_key = MIXTURE_FREE_SECRET_KEY
-    signature = Base64.encode64(
+    @signature = Base64.encode64(
         OpenSSL::HMAC.digest(
             OpenSSL::Digest::Digest.new('sha1'),
-            aws_secret_key, policy)
+            aws_secret_key, @policy)
         ).gsub("\n","")
+
+    @access_key = MIXTURE_FREE_ACCESS_KEY
      
-    @signature = "signature: #{signature}"
     slim :new_post
   end
 end
