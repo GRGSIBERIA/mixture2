2f33ba205829b49c586538b308834be0c3777b9e
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-03-02 05:10:05 +0900

    add vote and unvote

--- models/post_tag.rb
+++ models/post_tag.rb
@@ -8,4 +8,24 @@ class PostTag < Sequel::Model
     super
     
   end
+
+  def self.vote(post_id, tag_id)
+    post_tag = DB[:post_tags].where(past_id: post_id).where(tag_id: tag_id).first
+    if post_tag.nil? then
+      post_tag = PostTag.new(post_id: post_id, tag_id: tag_id, created_at: Time.now.to_s, updated_at: Time.now.to_s)
+    end
+    post_tag.count += 1
+    post_tag.save
+    post_tag
+  end
+
+  def self.unvote(post_tag_id)
+    ptag = PostTag.where(id: post_tag_id.to_i).first
+    if ptag.nil? then 
+      halt 400, "Cannot find post_tag_id(#{post_tag_id})."
+    end
+    ptag.count -= 1
+    ptag.save
+    ptag
+  end
 end
