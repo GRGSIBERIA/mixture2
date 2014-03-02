b2f36156670e835dc1ec72bd45ded709439e5c6c
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-03-02 22:27:43 +0900

    delete updated_at

--- migrate/001_create_categories.rb
+++ migrate/001_create_categories.rb
@@ -5,7 +5,6 @@ Sequel.migration do
       String :name, :size=>255, :null=>false
       Integer :count, :default=>0, :null=>false
       DateTime :created_at
-      DateTime :updated_at
       
       index [:name], :name=>:index_categories_on_name, :unique=>true
     end
