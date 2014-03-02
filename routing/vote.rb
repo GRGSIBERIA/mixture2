a2e465fc07812cd504141c1377f4a1f788ad14d8
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-03-02 21:36:40 +0900

    add routings

--- routing/vote.rb
+++ routing/vote.rb
@@ -2,18 +2,26 @@
 
 def routing_vote
   get '/vote/tag/new' do 
-
+    slim :new_tag
   end
 
   get '/vote/category/new' do 
-    
+    slim :new_category
+  end
+
+  post '/vote/category/create' do 
+
+  end
+
+  post '/vote/tag/create' do 
+
   end
 
   get '/vote/post/:post_id/:tag_name' do 
     post_id = params[:post_id]
     tag_name = params[:tag_name]
 
-    
+
   end
 
   get '/vote/category/:tag_id/:category_name' do 
