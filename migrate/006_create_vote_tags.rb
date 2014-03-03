eb2e84b7f934ed77b3a3cb4a75cc222f529748bb
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-03-04 04:11:45 +0900

    change migration

--- migrate/006_create_vote_tags.rb
+++ migrate/006_create_vote_tags.rb
@@ -3,12 +3,12 @@ Sequel.migration do
     
     create_table(:vote_tags, :ignore_index_errors=>true) do
       primary_key :id
+      Integer :vote_unvote, null: false
       DateTime :created_at
       
       foreign_key :user_id, :users, null: false
-      foreign_key :tag_id,  :tags,  null: false
       foreign_key :post_tag_id, :posts, null: false
-      index [:tag_id, :post_tag_id], :name=>:index_vote_tags_on_tag_id_and_post_tag_id
+      index [:post_tag_id], :name=>:index_vote_tags_on_post_tag_id
     end
   end
 end
