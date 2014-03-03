e747bece6b1b148a789819433888aa781d5b54b3
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-03-04 04:11:37 +0900

    refactoring tag

--- models/tag.rb
+++ models/tag.rb
@@ -7,7 +7,7 @@ class Tag < Sequel::Model
 
   def validate
     super
-    validates_presence [:name]
+    validates_presence [:name, :category_id]
 
     validates_unique :name
 
@@ -54,18 +54,9 @@ class Tag < Sequel::Model
 
   def self.vote_tag(tag_id, post_id, user_id)
     DB.transaction do 
-      vote_tag = VoteTag.create(tag_id, post_id, user_id)
-      
-      post_tag = PostTag.where(post_id, tag_id).first
-      if post_tag.nil? then
-        # 存在しない場合は新しく作成
-        post_tag = PostTag.new(post_id: post_id, tag_id: tag_id)
-        post_tag.count = 1
-        post_tag.save
-      else
-        # 存在する場合はアップデート
-        PostTag.where(id: post_tag.id).update(count: Sequel.+(:count, 1))
-      end
+      post_tag = PostTag.find_or_create(post_id, tag_id)
+
+
     end
   end
 end
