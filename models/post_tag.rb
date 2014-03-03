b118630d9aaa53dac72e356c684b690bab57f0a8
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-03-04 04:11:28 +0900

    add find_or_create for post_tag

--- models/post_tag.rb
+++ models/post_tag.rb
@@ -8,4 +8,18 @@ class PostTag < Sequel::Model
     super
     
   end
+
+  def self.find_or_create(post_id, tag_id)
+    post_tag = PostTag.where(post_id, tag_id).first
+    if post_tag.nil? then
+      # 存在しない場合は新しく作成
+      post_tag = PostTag.new(post_id: post_id, tag_id: tag_id)
+      post_tag.count = 1
+      post_tag.save
+    else
+      # 存在する場合はアップデート
+      post_tag = PostTag.where(id: post_tag.id).update(count: Sequel.+(:count, 1))
+    end
+    post_tag
+  end
 end
