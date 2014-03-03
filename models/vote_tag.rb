f9da0571bdec2543ef60201e61028835a316e835
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-03-04 04:11:56 +0900

    add validation and find_or_create

--- models/vote_tag.rb
+++ models/vote_tag.rb
@@ -8,5 +8,22 @@ class VoteTag < Sequel::Model
   def validate
     super
     
+    validates_presence [:user_id, :vote_unvote, :post_tag_id]
+
+    validates_includes [1, -1], :vote_unvote
+
+    validates_integer :user_id
+    validates_integer :vote_unvote
+    validates_integer :post_tag_id
+  end
+
+  def self.find_or_create(post_tag_id, user_id, vote_unvote)
+    vote_tag = VoteTag.where(post_tag_id: post_tag_id, user_id: user_id).first
+    unless vote_tag.nil? then
+      raise ArgumentError, "user(#{user_id}) was voted tag(#{tag_id}) to post(#{post_id})."
+    else
+      vote_tag = VoteTag.new(post_tag_id: post_tag_id, user_id: user_id, vote_unvote: vote_unvote, created_at: Time.now.to_s)
+
+    end
   end
 end
