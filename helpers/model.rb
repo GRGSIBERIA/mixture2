02b8f6db4e08c2882daceab6c8fe22f2db07dd9e
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-03-03 22:34:10 +0900

    modify

--- helpers/model.rb
+++ helpers/model.rb
@@ -1,8 +1,8 @@
 module Model
-    def save_to_validate(instance)
+  def save_to_validate(instance)
     instance.validate
     unless instance.valid? then
-      raise ArgumentError, instance.errors.join('\n')
+      raise ArgumentError, instance.errors.full_messages.join('<br>')
     end
     instance.save
   end
