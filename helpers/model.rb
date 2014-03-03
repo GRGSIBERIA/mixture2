ddec5262602f9155eb85aa99b0164f10a83da9d9
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-03-03 19:44:03 +0900

    delete some method

--- helpers/model.rb
+++ helpers/model.rb
@@ -1,41 +1,5 @@
 module Model
-  def countup(dbname, argument)
-    case argument
-    when Integer
-      DB[dbname].where(id: argument).update(Sequel.+(:count, 1))
-    when String
-      DB[dbname].where(name: argument).update(Sequel.+(:count, 1))
-    end
-  end
-
-  def countdown(dbname, argument)
-    case argument
-    when Integer
-      DB[dbname].where(id: argument).update(Sequel.-(:count, 1))
-    when String
-      DB[dbname].where(name: argument).update(Sequel.-(:count, 1))
-    end
-  end
-
-  def find(dbname, argument)
-    inst = nil
-    case argument
-    when Integer
-      inst = DB[dbname].where(id: argument).first
-    when String
-      if argument =~ /\A\d+\z/ then
-        inst = DB[dbname].where(id: argument.to_i).first
-      else
-        inst = DB[dbname].where(name: argument).first
-      end
-    end
-    if inst.nil? then
-      raise ArgumentError, "#{inst} is not found from #{dbname.to_s}."
-    end
-    inst
-  end
-
-  def save_to_validate(instance)
+    def save_to_validate(instance)
     instance.validate
     unless instance.valid? then
       raise ArgumentError, instance.errors.join('\n')
@@ -44,7 +8,4 @@ module Model
   end
 
   module_function :save_to_validate
-  module_function :countup
-  module_function :countdown
-  module_function :find
 end
