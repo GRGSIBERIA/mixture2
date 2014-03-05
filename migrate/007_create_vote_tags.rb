13e1b0ee17d5ac13d4f3df7b8cae9eae09e0e1fd
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-03-06 00:01:51 +0900

    rename for thruing migration

--- migrate/007_create_vote_tags.rb
+++ migrate/007_create_vote_tags.rb
@@ -6,8 +6,8 @@ Sequel.migration do
       Integer :vote_unvote, null: false
       DateTime :created_at
       
-      foreign_key :user_id, :users, null: false
-      foreign_key :post_tag_id, :posts, null: false
+      foreign_key :user_id,     :users,     null: false
+      foreign_key :post_tag_id, :post_tags, null: false
       unique [:user_id, :post_tag_id]
       index :post_tag_id
     end
