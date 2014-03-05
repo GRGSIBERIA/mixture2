db80e7da2036eb183e1a825a1a56619522cb6fa7
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-03-06 00:34:40 +0900

    modify user table

--- migrate/002_create_users.rb
+++ migrate/002_create_users.rb
@@ -3,13 +3,14 @@ Sequel.migration do
     
     create_table(:users, :ignore_index_errors=>true) do
       primary_key :id
-      String :name, :size=>255, :null=>false
-      String :nickname, :size=>255, :null=>false
-      String :password, :size=>255, :null=>false
+      String :name, :size=>128, :null=>false
+      String :nickname, :size=>128, :null=>false
+      String :password, :size=>66,  :null=>false
+      String :open_key, :size=>66,  :null=>false
       DateTime :created_at
-      DateTime :updated_at
       
-      index :name, unique: true
+      index :name,     unique: true
+      index :open_key, unique: true
     end
     
   end
