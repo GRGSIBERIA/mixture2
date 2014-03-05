dd691557e3a54c4c6704728b3e5b7f55b5e0047e
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-03-05 18:01:21 +0900

    debuged post parameters

--- routing/tag.rb
+++ routing/tag.rb
@@ -4,7 +4,6 @@ def routing_tag
     slim :new_tag
   end
 
-  # params tag_name
   post '/tag/create' do 
     tag_name = params[:tag_name]
     begin 
@@ -19,8 +18,6 @@ def routing_tag
     slim :change_category
   end
 
-  # params tag_id
-  # params category_name
   post '/tag/change/category' do 
     tag_id = params[:tag_id].to_i
     category_name = params[:category_name]
@@ -33,38 +30,19 @@ def routing_tag
   end
 
   # あくまで投票するだけで，追加はできない
-  # params post_id
-  # params user_id
-  # params tag_id
   post '/tag/vote' do 
     begin
-      tag_id = params[:tag_id].to_i
-      post_id = params[:post_id].to_i
       user_id = params[:user_id].to_i
-      PostTag.check_as_create(tag_id, post_id)
+      tag_id  = params[:tag_id].to_i
+      post_id = params[:post_id].to_i
+      PostTag.check_as_create(post_id, tag_id)
     rescue ArgumentError => e
       halt 400, e.message
     end
     "succeeded"
   end
 
-  get '/tag/vote' do 
+  get '/tag/vote_tag' do 
     slim :vote_tag
-  end
-
-  post '/tag/unvote' do 
-    begin
-      tag_id = params[:tag_id].to_i
-      post_id = params[:post_id].to_i
-      user_id = params[:user_id].to_i
-      Tag.unvote_tag(tag_id, post_id, user_id)
-    rescue ArgumentError => e
-      halt 400, e.message
-    end
-    "succeeded"
-  end
-
-  get '/tag/unvote' do 
-    slim :unvote_tag
   end
 end
