a3e3c56e8700e04d18decd5221bf865ee509ecf3
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-03-06 17:04:00 +0900

    add tag category

---
+++ models/tag_category.rb
@@ -0,0 +1,19 @@
+#-*- encoding: utf-8
+class TagCategory < Sequel::Model
+  plugin :validation_helpers
+  one_to_many :tag_categories
+
+  def validate
+    super
+  end
+
+  def self.find_create(tag_id, category_id)
+    TagCategory.find_or_create(tag_id: tag_id, category_id: category_id) { |c| 
+      c.tag_id = tag_id
+      c.category_id = category_id
+      c.created_at = Time.now.to_s
+      c.validate 
+      raise ArgumentError, c.errors.full_messages.join("<br>") unless c.valid?
+    }
+  end
+end
