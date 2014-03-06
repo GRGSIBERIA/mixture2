a1e5bd170cd325ae0d5be30b2b1b18471c5029cd
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-03-07 01:32:01 +0900

    move raise helper

--- helpers/exception.rb
+++ helpers/exception.rb
@@ -7,3 +7,15 @@ class InvalidAPIKey < StandardError; end
 def not_found_foreign_key(e)
   var = e.message.scan(/FOREIGN KEY \(`\w+/)[0].split("(`")[1]
 end
+
+def raise_helper(e, params)
+  begin
+    raise e, e.message
+  rescue ArgumentError => e
+    halt 400, e.message
+  rescue Sequel::ForeignKeyConstraintViolation => e 
+    var = not_found_foreign_key(e)
+    value = eval("params[:#{var}]")
+    halt 400, "#{var}(#{value}) is not found." #"#{var}(#{eval(params[:var])}) is not found."
+  end
+end
