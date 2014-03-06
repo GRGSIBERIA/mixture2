182781bc4e983b7206dcd3f3260a4a9d62fdc934
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-03-07 01:42:15 +0900

    add migration

---
+++ migrate/009_create_vote_categories.rb
@@ -0,0 +1,15 @@
+Sequel.migration do
+  up do
+    
+    create_table(:vote_categories, :ignore_index_errors=>true) do
+      primary_key :id
+      Integer :vote, null: false
+      DateTime :created_at
+      
+      foreign_key :user_id,     :users,     null: false
+      foreign_key :tag_category_id, :tag_categories, null: false
+      unique [:user_id, :tag_category_id]
+      index :tag_category
+    end
+  end
+end
