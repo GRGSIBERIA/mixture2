ac4ba1e079a248891c208d63bf7961fca2b96976
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-03-13 21:29:27 +0900

    add encrypt_password

--- helpers/crypt.rb
+++ helpers/crypt.rb
@@ -1,18 +1,42 @@
+#-*- encoding: utf-8
 require 'openssl'
 require 'base64'
 require "digest/sha2"
 
-module Crypt
-  def encrypt_password(password)
-    Digest::SHA256.hexdigest(PASSWORD_SALT + password)
+class Crypt
+  def self.encrypt(key, salt)
+    enc = OpenSSL::Cipher::Cipher.new("aes-256-cbc")
+    enc.encrypt
+    enc.pkcs5_keyivgen(salt)
+    bin = enc.update(key) + enc.final
+    Base64.encode64(bin)
   end
 
-  def make_apikey(user)
+  def self.decrypt(key, salt)
+    bin = Base64.decode64(key)
+    dec = OpenSSL::Cipher::Cipher.new("aes-256-cbc")
+    dec.decrypt
+    dec.pkcs5_keyivgen(salt)
+    dec.update(bin) + dec.final
+  end
+
+  def self.encrypt_password(password)
+    digest = password + PASSWORD_SALT
+    (0..4).each do |x|
+      digest = Digest::SHA256.hexdigest(digest + PASSWORD_SALT)
+    end
+    password = encrypt(digest, PASSWORD_SALT)
+  end
+
+  def self.check_password(password, decrypted)
+    password = encrypt_password(password)
+    decrypted = decrypt(decrypted, PASSWORD_SALT)
+    password == decrypted
+  end
+
+  def self.make_apikey(user)
     user_name = user.name
     password = user.password
     key_base = Digest::SHA256.hexdigest(password + user_name)
   end
-
-  module_function :make_apikey
-  module_function :encrypt_password
 end
