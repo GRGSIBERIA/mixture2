d9097be051698b5da4993bf01b7cb0058fb40d15
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-03-14 03:47:31 +0900

    add validate and entry code

--- models/developer.rb
+++ models/developer.rb
@@ -8,9 +8,21 @@ class Developer < Sequel::Model
   def validate
     super
 
-    validates_presence [:user_id, :secret_key, :consumer_key]
-    
+    validates_presence [:user_id, :secret_key, :consumer_key]    
+
     validates_unique :user_id
+
+    validates_integer :user_id
+    validates_string  :secret_key
+    validates_string  :consumer_key
   end
 
+  def entry(user_id)
+    user = User.find(user_id)
+    raise ArgumentError, "user_id(#{user_id}) is not found" if user.nil?
+    Developer.new(
+      user_id: user_id,
+      
+      )
+  end
 end
