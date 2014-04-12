5bc4e97c2df996e4daa04b6a926ad4e626797001
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-04-12 15:31:18 +0900

    configure production mode

--- app.rb
+++ app.rb
@@ -9,11 +9,11 @@ require 'yaml'
 require 'digest/sha2'
 
 INVALID_WORDS = %w(index home top help about security contact connect support faq form mail update mobile phone portal tour tutorial navigation navi manual doc company store shop topic news information info howto pr press release sitemap plan price business premium member term privacy rule inquiry legal policy icon image img photo css stylesheet style script src js javascript dist asset source static file flash swf xml json sag cgi account user item entry article page archive tag category event contest word product project download video blog diary site popular i my me owner profile self old first last start end special design theme purpose book read organization community group all status state search explore share feature upload rss atom widget api wiki bookmark captcha comment jump ranking setting config tool connect notify recent report system sys message msg log analysis query call calendar friend graph watch cart activity auth session register login logout signup forgot admin root secure get show create edit update post destroy delete remove reset error new dashboard recruit join offer career corp cgi-bin server-status balancer-manager ldap-status server-info svn as by if is on or add dir off out put case else find then when count order select switch developer dev test bug guest app maintenance roc id bot forum contribute usage feed ad service official repository spec asct dictionary dict ver gift alpha beta tux year public private default request req data master die exit eval undef nan null empty 0 www)
-#Encoding.default_external = Encoding::UTF_8  
-#Encoding.default_internal = Encoding::UTF_8  
+
 
 NUMBER_OF_CONTENTS_PER_PAGE = 20
 NUMBER_OF_WORDS_PER_PAGE = 50
+MIXTURE_S3_UPLOADS_ADDRESS = "https://s3-ap-northeast-1.amazonaws.com/mixture-posts/uploads/"
 
 # SequelでJSON出力ができるようになる
 class Sequel::Dataset
@@ -56,8 +56,11 @@ class Server < Sinatra::Base
     #encoding = "utf-8"
   end
 
+  configure :production do 
+    enable :sessions
+  end
+
   get '/' do 
-    puts User.find(name: "yamada", id: 1).name
     slim :index
   end
   
